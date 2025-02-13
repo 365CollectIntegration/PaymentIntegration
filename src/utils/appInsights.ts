51fa874ca0cb5c6d05c8ce801cbdc63078e93a57
Rex <jcadiao1102@gmail.com>
2025-02-13 14:21:20 +0800

    hard code keys

--- src/utils/appInsights.ts
+++ src/utils/appInsights.ts
@@ -2,8 +2,8 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 const appInsights = new ApplicationInsights({
   config: {
-    connectionString: process.env.NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING,
-    instrumentationKey: process.env.NEXT_PUBLIC_APPINSIGHTS_INSTRUMENTATIONKEY,
+    connectionString: 'InstrumentationKey=32a1eeb6-c651-43fc-a90c-f26290929266;IngestionEndpoint=https://australiasoutheast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://australiasoutheast.livediagnostics.monitor.azure.com/;ApplicationId=b4022679-e693-44cd-9d89-d077f5249125',
+    instrumentationKey: '32a1eeb6-c651-43fc-a90c-f26290929266',
     enableAutoRouteTracking: true, // Tracks page views
   }
 });
